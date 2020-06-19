66d21877bc519f1b140ed0f85903409846d08e8c
David <davidcantumartinez@gmail.com>
2020-06-19 13:46:36 -0500

    ya quedo

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,44 @@ function init(){
 		method: 'GET'
 	}
 
+	document.getElementById("post6").addEventListener("click", function(e) {
+		var id  = document.getElementById("Name6").value;
+		var dExp = document.getElementById("dExp").value;
+
+
+		console.log(typeof(dExp))
+
+		let url2 = '/api/finanzasAn1'
+		let settings2 = {
+			method: 'POST',
+			body: JSON.stringify({
+				id: id,
+				exp: dExp
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+		}
+
+		fetch(url2, settings2)
+		.then( response => {
+			if ( response.ok ){
+				return response.json();
+			}
+
+			throw new Error ( response.statusText );
+		})
+		.then( responseJSON => {
+
+			console.log(responseJSON);
+		})
+		.catch( err => {
+			console.log( err );
+		})
+		// alert(document.getElementById("tiempoini").value);
+	});
+
+
 
 
 	fetch(url, settings)
